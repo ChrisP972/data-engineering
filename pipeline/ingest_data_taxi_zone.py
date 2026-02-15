32f3963dce99a004363eb13d6a68a681e421c0f3
ChrisP972 <Chris9713@hotmail.co.uk>
2026-02-15 17:29:47 +0000

    ingest_taxi_data_zone updates

--- pipeline/ingest_data_taxi_zone.py
+++ pipeline/ingest_data_taxi_zone.py
@@ -17,38 +17,19 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
     # Ingest NYC taxi data into PostgreSQL database
     url = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
 
-        # Database connection parameters
+    # Database connection parameters
     engine = create_engine(
         f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
     )
 
     df = pd.read_csv(url)
 
-    df_to_sql(
+    df.to_sql(
         name=target_table,
         con=engine,
         if_exists='replace',
-        
-    )
-
-    first = True
-
-    for df_chunk in tqdm(df_iter):
-        if first:
-            df_chunk.head(0).to_sql(
-                name=target_table, 
-                con=engine, 
-                if_exists='replace'
-            )
-            
-            print("\nTable Created")        
-            first = False
-
-        df_chunk.to_sql(
-            name=target_table,
-            con=engine,
-            if_exists='append'
         )
-            
+
+                
 if __name__ == '__main__':
     run()
